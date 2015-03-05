760c6c18a8651927fd4f0cfab069751c060062ee
jhost84 <jhost84@gmail.com>
2015-03-05 18:56:24 -0500

    d

--- L7/js/grayscale.js
+++ L7/js/grayscale.js
@@ -24,22 +24,7 @@ $(function() {
     });
 });
 
-$('.thumbnail').click(function(){
-    $('.modal-body').empty();
-    var title = $(this).parent('a').attr("title");
-    $('.modal-title').html(title);
-    $($(this).parents('div').html()).appendTo('.modal-body');
-    $('#myModal').modal({show:true});
-});
 
-/* blur on modal open, unblur on close */
-$('#myModal').on('show.bs.modal', function () {
-   $('.col-6,.row .thumbnail').addClass('blur');
-})
-
-$('#myModal').on('hide.bs.modal', function () {
-   $('.col-6,.row .thumbnail').removeClass('blur');
-})
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
